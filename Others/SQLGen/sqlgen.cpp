495c04267340a3346b937a2dac64cdf2a13247d6
Zhe Wang <wazh@microsoft.com>
2022-08-12 15:37:25 -0700

    update code to simplify VS debugging for sqlgen

--- Others/SQLGen/sqlgen.cpp
+++ Others/SQLGen/sqlgen.cpp
@@ -5,55 +5,62 @@
 #include "llvm/TableGen/Error.h"
 #include "llvm/TableGen/Main.h"
 #include "llvm/TableGen/Record.h"
-
 #include "Error.h"
+#include <iostream>
 
 using namespace llvm;
 
 namespace {
-enum ActionType {
-  PrintRecords,
-  PrintDetailedRecords,
-  EmitSQL
-};
+    enum ActionType {
+        PrintRecords,
+        PrintDetailedRecords,
+        EmitSQL
+    };
 } // end anonymous namespace
 
 static cl::opt<ActionType> Action(
     cl::desc("Actions to perform"),
     cl::values(
-      clEnumValN(PrintRecords, "print-records", ""),
-      clEnumValN(PrintDetailedRecords, "print-detailed-records", ""),
-      clEnumValN(EmitSQL, "emit-sql", "")
+        clEnumValN(PrintRecords, "print-records", ""),
+        clEnumValN(PrintDetailedRecords, "print-detailed-records", ""),
+        clEnumValN(EmitSQL, "emit-sql", "")
     ),
     cl::init(EmitSQL));
 
-Error emitSQL(raw_ostream &OS, RecordKeeper &Records);
+Error emitSQL(raw_ostream& OS, RecordKeeper& Records);
 
-bool SQLGenMain(raw_ostream &OS, RecordKeeper &Records)
+bool SQLGenMain(raw_ostream& OS, RecordKeeper& Records)
 {
-  switch (Action)
-  {
-  case PrintRecords:
-    OS << Records;
-    break;
-  case EmitSQL:
-    if (auto E = emitSQL(OS, Records))
+    switch (Action)
     {
-      handleAllErrors(std::move(E),
-        [](const SMLocError &E) { llvm::PrintError(E.Loc.getPointer(), E.getMessage()); },
-        [](const ErrorInfoBase &E) { E.log(WithColor::error()); errs() << "\n"; });
-      return true;
+    case PrintRecords:
+        OS << Records;
+        break;
+    case EmitSQL:
+        if (auto E = emitSQL(OS, Records))
+        {
+            handleAllErrors(std::move(E),
+                [](const SMLocError& E) { llvm::PrintError(E.Loc.getPointer(), E.getMessage()); },
+                [](const ErrorInfoBase& E) { E.log(WithColor::error()); errs() << "\n"; });
+            return true;
+        }
+        break;
+    default:
+        return true;
     }
-    break;
-  default:
-    return true;
-  }
 
-  return false;
+    return false;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  cl::ParseCommandLineOptions(argc, argv);
-  return llvm::TableGenMain(argv[0], &SQLGenMain);
+    // Add an ugly default for convenient debugging: sqlgen.exe --emit-sql InsertRows.td
+    std::vector<char*> params = {const_cast<char*>("sqlgen.exe"), const_cast<char*>("--emit-sql"), const_cast<char*>("InsertRows.td")};
+    if (argc < 2)
+    {
+        argc = 3;
+        argv = params.data();
+    }
+    cl::ParseCommandLineOptions(argc, argv);
+    return llvm::TableGenMain(argv[0], &SQLGenMain);
 }
