0de8550baa5b6eaa93c302f71d617352b0e1354d
Zhe Wang <wazh@microsoft.com>
2022-09-18 16:12:06 -0700

    simple clean up

--- Others/mlir-crash-mapping/main.cpp
+++ Others/mlir-crash-mapping/main.cpp
@@ -4,7 +4,8 @@
 #include "mlir/IR/BlockAndValueMapping.h"
 #include <iostream>
 
-int main() {
+int main()
+{
     mlir::MLIRContext context;
     context.loadDialect<mlir::arith::ArithmeticDialect>();
     context.loadDialect<mlir::test::TestDialect>();
@@ -14,16 +15,12 @@ int main() {
 
     mlir::Value intOperand = builder.create<mlir::arith::ConstantOp>(loc, builder.getI64IntegerAttr(57));
     auto testOp = builder.create<mlir::test::TestOp>(loc, intOperand.getType(), intOperand);
-
     auto floatValue = builder.create<mlir::arith::ConstantOp>(loc, builder.getF64FloatAttr(57));
 
     mlir::BlockAndValueMapping mapping;
     mapping.map(intOperand, floatValue);
 
     mapping.lookup(testOp.typedOperand()).dump();
-
-    // Workaround:
-    //mapping.lookup(mlir::Value(testOp.typedOperand())).dump();
 
     return 0;
 }
